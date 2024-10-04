c03045da28862f52aad77b8049d8ca8547036fdf
Ines Montero <monteroi@banfondesa.com.do>
2024-10-04 14:32:09 -0400

    feat: add style for feed's components, testing data to be rendering and test image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,125 @@
 import Image from "next/image";
+import ExperienceCard from "./components/ExperienceCard";
+import { IExperienceCard } from "./components/ExperienceCard/types";
+import SocialContact from "./components/SocialContact";
+import { IProjectCard } from "./components/ProjectCard/types";
+import ProjectCard from "./components/ProjectCard";
 
+const experienceCards: IExperienceCard[] = [
+  {
+    title: 'numero 1',
+    desc: 'xxxxxxx',
+    dateRange: 'Jun 2021 - Present',
+    link: '#',
+    tags: [ 
+      {
+        title: 'html'
+      },
+      {
+        title: 'css'
+      }
+    ]
+  },
+  {
+    title: 'numero 2',
+    desc: 'xxxxxxx 2',
+    dateRange: 'Jun 2022 - Present',
+    link: '#',
+    tags: [ 
+      {
+        title: 'html2'
+      },
+      {
+        title: 'css2'
+      }
+    ]
+  }
+]
+
+const projectCards: IProjectCard[] = [
+  {
+    title: 'first project',
+    desc: 'this is the desc of the first project',
+    link: '#',
+    image: {
+      src: '/img/logo_dog_220x.avif',
+      alt: 'first project image'
+    }
+  },
+  {
+    title: 'second project',
+    desc: 'this is the desc of the second project',
+    link: '#',
+    image: {
+      src: '/img/logo_dog_220x.avif',
+      alt: 'second project image'
+    }
+  },
+]
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <div className="lg:flex lg:justify-between lg:gap-4">
+          <div className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-20">
+            {projectCards.map((item, index) => (
+              <div key={index} className='mb-4'>
+                <ProjectCard
+                  title={item.title}
+                  desc={item.desc}
+                  image={item.image}
+                  link={item.link}
+                />
+              </div>
+            ))}
+            <SocialContact/>
+          </div>
+          <div className="py-20">
+            {experienceCards.map((item, index) => (
+              <div key={index} className='mb-4'>
+                <ExperienceCard
+                  title={item.title}
+                  desc={item.desc}
+                  dateRange={item.dateRange}
+                  tags={item.tags}
+                  link={item.link}
+                />
+              </div>
+            ))}
+            {experienceCards.map((item, index) => (
+              <div key={index} className='mb-4'>
+                <ExperienceCard
+                  title={item.title}
+                  desc={item.desc}
+                  dateRange={item.dateRange}
+                  tags={item.tags}
+                  link={item.link}
+                />
+              </div>
+            ))}
+            {experienceCards.map((item, index) => (
+              <div key={index} className='mb-4'>
+                <ExperienceCard
+                  title={item.title}
+                  desc={item.desc}
+                  dateRange={item.dateRange}
+                  tags={item.tags}
+                  link={item.link}
+                />
+              </div>
+            ))}
+            {experienceCards.map((item, index) => (
+              <div key={index} className='mb-4'>
+                <ExperienceCard
+                  title={item.title}
+                  desc={item.desc}
+                  dateRange={item.dateRange}
+                  tags={item.tags}
+                  link={item.link}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
