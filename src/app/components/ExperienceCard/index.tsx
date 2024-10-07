c52fc589e9e3e14aceae29e54efdb4551d393b22
Ines Montero <monteroi@banfondesa.com.do>
2024-10-07 15:32:47 -0400

    feat: modify based component styles for feed

--- src/app/components/ExperienceCard/index.tsx
+++ src/app/components/ExperienceCard/index.tsx
@@ -8,7 +8,7 @@ const ExperienceCard = ({dateRange, title, desc, tags, link}: IExperienceCard) =
     <div>
         {title && dateRange && desc ? (
         <Link href={link} className='experienceCard flex flex-col hover:text-secondary rounded-2xl p-6 hover:ring-2 hover:ring-secondary'>
-            <h3 className='font-bold  flex items-center justify-between'>
+            <h3 className='font-bold flex items-center justify-between text-base'>
                 <span>{title}</span>
                 <svg className='svgIcon -mt-1' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
@@ -16,10 +16,10 @@ const ExperienceCard = ({dateRange, title, desc, tags, link}: IExperienceCard) =
                     <line x1="10" y1="14" x2="21" y2="3"></line>
                 </svg>
             </h3>
-            <span className='font-semibold italic'>
+            <span className='font-semibold italic text-xs'>
                 {dateRange}
             </span>
-            <div className='mt-5'>{desc}</div>
+            <div className='mt-5 text-sm'>{desc}</div>
             <ul className='mt-5 flex flex-wrap'>
                 {tags.map((tag, index) => (
                     <li className="mt-2 ring-1 ring-inset ring-purple-700/10 bg-purple-200 text-purple-800 text-xs font-medium me-2 px-3 py-1.5 rounded-full" key={index}>
--- src/app/components/ProjectCard/index.tsx
+++ src/app/components/ProjectCard/index.tsx
@@ -7,12 +7,12 @@ const ProjectCard = ({ title, desc, image, link}: IProjectCard) => {
   return (
     <div>
         {title && image && desc && link ? (
-            <Link href={link} className="projectCard ring ring-2 ring-gray-300 flex flex-col lg:flex-row gap-5 overflow-hidden before:ease-in-out after:ease-in-out bg-background group cursor-pointer relative flex flex-col gap-4 justify-between rounded-2xl border hover:after:w-full border-foreground-222 hover:border-purple-200 duration-300 p-4 md:p-6 px-8 before:h-full before:w-2 hover:before:w-full after:absolute after:top-0 after:left-0 after:h-full after:w-0 after:duration-300 after:opacity-5  before:duration-300 before:-z-1 before:bg-purple-200 before:absolute before:top-0 before:left-0">
-                <div className='font-semibold font-medium duration-300 group-hover:text-foreground group-hover:z-[5]' >
+            <Link href={link} className="projectCard items-center ring ring-2 ring-gray-300 flex flex-col lg:flex-row overflow-hidden before:ease-in-out after:ease-in-out bg-background group cursor-pointer relative flex flex-col gap-4 justify-between rounded-2xl border hover:after:w-full border-foreground-222 hover:border-purple-200 duration-300 p-4 md:p-6 px-8 before:h-full before:w-2 hover:before:w-full after:absolute after:top-0 after:left-0 after:h-full after:w-0 after:duration-300 after:opacity-5  before:duration-300 before:-z-1 before:bg-purple-200 before:absolute before:top-0 before:left-0">
+                <div className='flex items-center duration-300 group-hover:text-foreground group-hover:z-[5] bg-white w-fit py-2 px-3.5 h-fit flex items-center'>
                     <FrameImage src={image.src} alt={image.alt} width={image.width} height={image.height} className={image.className}></FrameImage>
                 </div>
-                <div className='font-medium text-xs duration-300 group-hover:text-purple-800 group-hover:z-[5]'>
-                    <h3 className='font-semibold mb-3 flex justify-between items-center'>
+                <div className='font-medium text-xs duration-300 group-hover:text-purple-800 group-hover:z-[5] lg:w-[80%]'>
+                    <h3 className='font-semibold mb-3 flex justify-between items-center text-base'>
                         <span>{title}</span>
                         <svg className='svgIcon' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
@@ -20,7 +20,7 @@ const ProjectCard = ({ title, desc, image, link}: IProjectCard) => {
                             <line x1="10" y1="14" x2="21" y2="3"></line>
                         </svg>
                     </h3>
-                    <div>{desc}</div>
+                    <div className='text-sm'>{desc}</div>
                 </div>
             </Link>
         ) : (
