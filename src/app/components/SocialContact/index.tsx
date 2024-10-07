cb8449399368cd81fc8a012b557d8e71221bb2a4
ines-sys <inesmariaconcepcionpink@gmail.com>
2024-10-06 22:18:23 -0400

    feat: remove ws contact, also modify colors

--- src/app/components/SocialContact/index.tsx
+++ src/app/components/SocialContact/index.tsx
@@ -1,30 +1,22 @@
 const SocialContact = () => {
     return (
         <div className="socialCard flex items-center justify-center p-6 gap-5 bg-transparent">
-            <a className="socialContainer containerOne w-10 h-10 rounded-full bg-purple-800 flex items-center justify-center overflow-hidden transition duration-1000" href="mailto:inesmariaconcepcion@gmail.com" title='inesmariaconcepcion@gmail.com'>
+            <a className="socialContainer containerOne w-10 h-10 rounded-full bg-primary flex items-center justify-center overflow-hidden transition duration-1000" href="mailto:inesmariaconcepcion@gmail.com" title='inesmariaconcepcion@gmail.com'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="socialSvg w-4">
                     <path d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z" />
                 </svg>
             </a>
 
-            <a className="socialContainer containerTwo w-10 h-10 rounded-full bg-purple-800 flex items-center justify-center overflow-hidden transition duration-1000" href="https://github.com/ines-sys" title="ines-sys">
+            <a className="socialContainer containerTwo w-10 h-10 rounded-full bg-primary flex items-center justify-center overflow-hidden transition duration-1000" href="https://github.com/ines-sys" title="ines-sys">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" className="socialSvg w-4">
                     <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
                 </svg>
             </a>
 
-            <a className="socialContainer containerThree w-10 h-10 rounded-full bg-purple-800 flex items-center justify-center overflow-hidden transition duration-1000" href="https://www.linkedin.com/in/in%C3%A9s-mar%C3%ADa-montero-concepci%C3%B3n-6002752bb" title="Ines Maria M. C.">
+            <a className="socialContainer containerThree w-10 h-10 rounded-full bg-primary flex items-center justify-center overflow-hidden transition duration-1000" href="https://www.linkedin.com/in/in%C3%A9s-mar%C3%ADa-montero-concepci%C3%B3n-6002752bb" title="Ines Maria M. C.">
                 <svg viewBox="0 0 448 512" className="socialSvg w-4">
                     <path
                         d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                    ></path>
-                </svg>
-            </a>
-
-            <a className="socialContainer containerFour w-10 h-10 rounded-full bg-purple-800 flex items-center justify-center overflow-hidden transition duration-1000" href="#">
-                <svg viewBox="0 0 16 16" className="socialSvg w-4">
-                    <path
-                        d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.374-.922l-.242-.144-2.494.654.667-2.428-.157-.25a6.55 6.55 0 0 1-1.024-3.551c.002-3.637 2.96-6.594 6.602-6.594a6.606 6.606 0 0 1 6.594 6.594c-.002 3.637-2.96 6.594-6.594 6.594zm3.61-4.81c-.198-.1-1.174-.578-1.356-.644-.182-.067-.315-.1-.446.1-.13.198-.514.645-.63.777-.115.13-.23.147-.426.048-.198-.1-.838-.308-1.595-.983-.59-.526-.987-1.175-1.104-1.373-.115-.198-.013-.305.087-.405.09-.088.198-.23.298-.347.1-.115.13-.198.198-.33.067-.13.033-.248-.017-.347-.048-.1-.447-1.08-.61-1.471-.16-.39-.32-.335-.446-.335h-.385c-.13 0-.347.048-.528.248-.182.198-.696.678-.696 1.654s.712 1.918.812 2.05c.1.13 1.402 2.15 3.396 3.015.474.198.844.315 1.13.403.476.152.91.13 1.255.079.382-.058 1.175-.479 1.34-.94.165-.462.165-.86.115-.94-.048-.082-.182-.13-.38-.23z"
                     ></path>
                 </svg>
             </a>
