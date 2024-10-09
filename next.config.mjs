770232a0589c65354df4ca9e02e8a43804408fea
Ines Montero <monteroi@banfondesa.com.do>
2024-10-09 13:58:19 -0400

    build: add basePath on next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
+    basePath: '/portafolio',
     images: {
         unoptimized: true, // Desactiva la optimización de imágenes
     },
