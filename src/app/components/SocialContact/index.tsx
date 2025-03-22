50239e0427568b4f4fb7ca6107d5763a48d9a050
Inés María Montero Concepción <85138994+ines-sys@users.noreply.github.com>
2025-03-21 21:53:19 -0400

    Update index.tsx

--- src/app/components/SocialContact/index.tsx
+++ src/app/components/SocialContact/index.tsx
@@ -1,7 +1,7 @@
 const SocialContact = () => {
     return (
         <div className="socialCard flex items-center justify-center p-6 gap-5 bg-transparent">
-            <a className="socialContainer containerOne w-10 h-10 rounded-full bg-primary flex items-center justify-center overflow-hidden transition duration-1000" href="mailto:inesmariaconcepcion@gmail.com" title='inesmariaconcepcion@gmail.com'>
+            <a className="socialContainer containerOne w-10 h-10 rounded-full bg-primary flex items-center justify-center overflow-hidden transition duration-1000" href="mailto:inesmariaconcepcionpink@gmail.com" title='inesmariaconcepcion@gmail.com'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="socialSvg w-4">
                     <path d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z" />
                 </svg>
